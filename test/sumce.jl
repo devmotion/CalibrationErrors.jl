971ea33215c3d46285bbe33208219c2c78aa1ef3
David Widmann <dev+git@devmotion.de>
2020-06-18 14:40:17 +0200

    Rename UCME

--- test/sumce.jl
+++ test/sumce.jl
@@ -7,19 +7,21 @@ using Test
 
 Random.seed!(1234)
 
-@testset "SUMCE: Two-dimensional example" begin
+@testset "UCME: Two-dimensional example" begin
     # three test locations
-    sumce = SUMCE(transform(SqExponentialKernel(), 2), WhiteKernel(),
-                  [[1.0, 0], [0.5, 0.5], [0.0, 1]], [1, 1, 2])
+    ucme = UCME(transform(SqExponentialKernel(), 2), WhiteKernel(),
+                [[1.0, 0], [0.5, 0.5], [0.0, 1]], [1, 1, 2])
 
     # two predictions
-    @test @inferred(calibrationerror(sumce, ([1 0; 0 1], [1, 2]))) ≈ 0
-    @test @inferred(calibrationerror(sumce, [1 0; 0 1], [1, 1])) ≈ (exp(-16) + exp(-4) + 1) / 12
-    @test @inferred(calibrationerror(sumce, [[1, 0], [0, 1]], [2, 1])) ≈ (1 - exp(-8))^2 / 6
-    @test @inferred(calibrationerror(sumce, ([1 0; 0 1], [2, 2]))) ≈ (exp(-16) + exp(-4) + 1) / 12
+    @test @inferred(calibrationerror(ucme, ([1 0; 0 1], [1, 2]))) ≈ 0
+    @test @inferred(calibrationerror(ucme, [1 0; 0 1], [1, 1])) ≈
+        (exp(-16) + exp(-4) + 1) / 12
+    @test @inferred(calibrationerror(ucme, [[1, 0], [0, 1]], [2, 1])) ≈ (1 - exp(-8))^2 / 6
+    @test @inferred(calibrationerror(ucme, ([1 0; 0 1], [2, 2]))) ≈
+        (exp(-16) + exp(-4) + 1) / 12
 end
 
-@testset "SUMCE: Basic properties" begin
+@testset "UCME: Basic properties" begin
     estimates = Vector{Float64}(undef, 1_000)
 
     for ntest in (1, 5, 10), nclasses in (2, 10, 100)
@@ -27,13 +29,14 @@ end
 
         testpredictions = [rand(dist) for _ in 1:ntest]
         testtargets = rand(1:nclasses, ntest)
-        sumce = SUMCE(transform(ExponentialKernel(), 0.1), WhiteKernel(), testpredictions, testtargets)
+        ucme = UCME(transform(ExponentialKernel(), 0.1), WhiteKernel(),
+                    testpredictions, testtargets)
 
         for i in 1:length(estimates)
             predictions = [rand(dist) for _ in 1:20]
             targets = [rand(Categorical(p)) for p in predictions]
 
-            estimates[i] = calibrationerror(sumce, predictions, targets)
+            estimates[i] = calibrationerror(ucme, predictions, targets)
         end
 
         @test all(x > zero(x) for x in estimates)
