d626f7ea8bb858283f15a5f8ade2f39be6049cd5
David Widmann <dev+git@devmotion.de>
2020-06-22 09:55:38 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.5

---
+++ src/CalibrationErrorsDistributions.jl
@@ -0,0 +1,5 @@
+module CalibrationErrorsDistributions
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using CalibrationErrorsDistributions
+using Test
+
+@testset "CalibrationErrorsDistributions.jl" begin
+    # Write your tests here.
+end
