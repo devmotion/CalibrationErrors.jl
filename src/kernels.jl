cb21ff81fe1c008e794ef7dd23dc0940ae77953d
David Widmann <dev+git@devmotion.de>
2020-08-20 12:15:10 +0200

    More efficient evaluation of the TV distance for scaled kernels

--- src/kernels.jl
+++ src/kernels.jl
@@ -3,3 +3,8 @@ struct TVExponentialKernel <: KernelFunctions.SimpleKernel end
 
 KernelFunctions.kappa(kernel::TVExponentialKernel, d) = exp(-d)
 KernelFunctions.metric(::TVExponentialKernel) = TotalVariation()
+
+# Simplifies computations of distances but is type piracy... :(
+function KernelFunctions._scale(t::ScaleTransform, metric::TotalVariation, x, y)
+    return first(t.s) * evaluate(metric, x, y)
+end
