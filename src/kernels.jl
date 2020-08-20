20a4686dceb9f49684627c293546a2f8d898aa52
David Widmann <dev+git@devmotion.de>
2020-08-20 11:22:57 +0200

    Fix implementation of TVExponentialKernel

--- src/kernels.jl
+++ src/kernels.jl
@@ -1,23 +1,5 @@
 # exponential kernel with total variation distance
 struct TVExponentialKernel <: KernelFunctions.SimpleKernel end
 
-# TODO: Not needed in KernelFunctions 1.0
-function (kernel::TVExponentialKernel)(x, y)
-    return kappa(kernel, evaluate(KernelFunctions.metric(kernel), x, y))
-end
-
 KernelFunctions.kappa(kernel::TVExponentialKernel, d) = exp(-d)
 KernelFunctions.metric(::TVExponentialKernel) = TotalVariation()
-
-# more efficient application of ScaleTransform
-# Optimizations for scale transforms of simple kernels to save allocations:
-# Instead of a multiplying every element of the inputs before evaluating the metric,
-# we perform a scalar multiplcation of the distance of the original inputs, if possible.
-function (k::TransformedKernel{<:TVExponentialKernel,<:ScaleTransform})(
-    x::AbstractVector{<:Real},
-    y::AbstractVector{<:Real},
-)
-    return kappa(k.kernel,
-                 KernelFunctions._scale(k.transform,
-                                        KernelFunctions.metric(k.kernel), x, y))
-end
